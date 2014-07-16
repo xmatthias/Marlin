39412e19cff2b8efb0c56f6ff43c60a190cab488
Matthias Voppichler <matthias.voppichler@gmail.com>
2014-07-16 22:34:53 +0200

    fixed height for 0.35-nozzle

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -457,7 +457,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
 // For deltabots this means top and center of the Cartesian print volume.
 #define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS 0
-#define MANUAL_Z_HOME_POS 257.8  // For delta: Distance between nozzle and print surface after homing.
+#define MANUAL_Z_HOME_POS 259.2  // For delta: Distance between nozzle and print surface after homing.
 
 //// MOVEMENT SETTINGS
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
